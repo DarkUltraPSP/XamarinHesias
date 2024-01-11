dd2a2150220545b9cd67cf5df4113cbbf33e52b2
DarkUltraPSP <darkultrapsp.mc@gmail.com>
2024-01-12 00:24:19 +0100

    non saved file

--- CRUD/ContactDatabaseService.cs
+++ CRUD/ContactDatabaseService.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+using CRUD.Models;
 
 namespace CRUD
 {
-    internal class SQLiteHelper
+    public class ContactDatabaseService
     {
+        readonly SQLiteConnection database;
+
+        public ContactDatabaseService(string dbPath)
+        {
+            database = new SQLiteConnection(dbPath);
+            database.CreateTable<Contact>();
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return database.Table<Contact>().ToList();
+        }
+
+        public Contact GetContact(int id)
+        {
+            return database.Get<Contact>(id);
+        }
+
+        public int SaveContact(Contact contact)
+        {
+            if (contact.Id != 0)
+            {
+                database.Update(contact);
+                return contact.Id;
+            }
+            else
+            {
+                return database.Insert(contact);
+            }
+        }
+
+        public int DeleteContact(Contact contact)
+        {
+            return database.Delete(contact);
+        }
     }
 }
