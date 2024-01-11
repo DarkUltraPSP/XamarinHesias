5519a75c5ab633e2808cff271b7ed29f432d052e
DarkUltraPSP <darkultrapsp.mc@gmail.com>
2024-01-12 00:22:33 +0100

    first commit

---
+++ CRUD/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace CRUD.Models
+{
+    internal class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Commentaire { get; set; }
+    }
+}
